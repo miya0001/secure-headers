932bc51e67a6fa0508a81e0473bd7f709360fabd
miya0001 <miya@wpist.me>
2017-02-18 17:05:51 +0900

    first commit

---
+++ secure-headers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Secure Headers
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     secure-headers
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Secure_Headers
+ */
